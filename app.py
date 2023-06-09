bb2fa11fd19dd8a49ef5f68d5ddf9fb2c6fea70e
Tolu Joel <toluisjoel@gmail.com>
2023-06-09 20:55:05 +0100

    Added file extensions to app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import os
+
 from chunkers import image_split, text_split, video_split, pdf_split, archive_split
+
+file_extensions = {
+    'image': ['.jpg', '.jpeg', '.png', '.webp', '.svg'],
+    'archive': ['.tar', '.gz', '.zip'],
+    'video': ['.mp4', '.mkv'],
+    'text': ['.txt'],
+    'pdf': ['.pdf'],
+}
