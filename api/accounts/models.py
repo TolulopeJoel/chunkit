eec90539e39191a4877d09e85430b702c48a8a32
Tolu Joel <toluisjoel@gmail.com>
2023-09-27 02:07:08 +0100

    - Added custom user model - Added UserListView and endpoint

--- api/accounts/models.py
+++ api/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+import uuid
+
+class CustomUser(AbstractUser):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+        unique=True
+    )
+    
--- api/accounts/views.py
+++ api/accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserListView(generics.ListAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
