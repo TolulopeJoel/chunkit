6677f597a501e75a361d9f60788d6a41c8ee55f8
Tolu Joel <toluisjoel@gmail.com>
2023-10-09 02:19:56 +0100

    Add JWT token endpoints

--- api/FileChunker/urls.py
+++ api/FileChunker/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('files.urls')),
-    path('api/', include('accounts.urls')),
+    path('api/auth/', include('accounts.urls')),
 ]
