2961f8a62497efd5967178410888cf3c6be8b075
Tolu Joel <toluisjoel@gmail.com>
2023-11-15 14:22:39 +0100

    Move split_image function from services.py to utils.py

--- api/files/utils.py
+++ api/files/utils.py
@@ -1,4 +1,8 @@
+import os
 from datetime import datetime
+
+import requests
+from PIL import Image
 
 
 def get_folder_path(root_path):
@@ -7,7 +11,6 @@ def get_folder_path(root_path):
     on the current date and week number.
     """
 
-    # Current date and time
     now = datetime.now()
 
     # Calculate the week number of current month
@@ -15,3 +18,62 @@ def get_folder_path(root_path):
     week_number = (day_of_month - 1) // 7 + 1
 
     return f'media/{root_path}/{now.year}/{now.month}/{week_number}'
+
+
+def split_image(object, num_chunks=2):
+    """
+    Split an image into multiple chunks.
+
+    Args:
+        object: The chunk object containing the uploaded file.
+        num_chunks: The number of chunks to split the image into. Default is 2
+
+    Returns:
+        List[str]: A list of file paths for the generated image chunks.
+
+    Examples:
+        >>> split_image(object, num_chunks=2)
+        [
+            '/path/to/image_chunks/image.chunk1.png',
+            '/path/to/image_chunks/image.chunk2.png'
+        ]
+    """
+
+    image_file = object.uploaded_file
+
+    image = Image.open(requests.get(image_file.file.url, stream=True).raw)
+    image_width, image_height = image.size
+
+    num_chunks_vertical = int(num_chunks**0.5)
+    num_chunks_horizontal = num_chunks // num_chunks_vertical
+
+    chunk_width = (
+        image_width + num_chunks_horizontal - 1
+    ) // num_chunks_horizontal
+    chunk_height = (
+        image_height + num_chunks_vertical - 1
+    ) // num_chunks_vertical
+
+    chunks_folder = f"{image_file.name}_chunks"
+    os.makedirs(chunks_folder, exist_ok=True)
+
+    count = 0
+    chunk_files = []
+
+    for y_pixels in range(num_chunks_vertical):
+        for x_pixels in range(num_chunks_horizontal):
+            left = x_pixels * chunk_width
+            upper = y_pixels * chunk_height
+            right = min((x_pixels + 1) * chunk_width, image_width)
+            lower = min((y_pixels + 1) * chunk_height, image_height)
+
+            chunk = image.crop((left, upper, right, lower))
+            chunk_file_path = os.path.join(
+                chunks_folder,
+                f"{image_file.name}.chunk{count+1}.{image_file.type}",
+            )
+            chunk.save(chunk_file_path)
+            chunk_files.append(chunk_file_path)
+            count += 1
+
+    return chunk_files
