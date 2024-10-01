3622b3c60f9108899e554e4b6b27a4374de9ded2
Tolu Joel <toluisjoel@gmail.com>
2024-10-01 23:21:04 +0100

    chore: Delete API related directories and files

--- api/files/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Chunk, UploadedFile
-
-class FileAdmin(admin.ModelAdmin):
-    list_display = ['name', 'user', 'uploaded_at']
-
-admin.site.register(UploadedFile, FileAdmin)
-admin.site.register(Chunk)
--- api/files/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('chunks/<int:file_id>/', views.ChunkListView.as_view(), name='chunks-list'),
-    path('chunks/', views.ChunkCreateView.as_view(), name='chunks-create'),
-    path('uploaded-files/', views.UploadedFileListCreateView.as_view(), name='files-list-create'),
-]
